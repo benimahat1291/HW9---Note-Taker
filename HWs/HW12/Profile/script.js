9912145fd79ab3aa28cee291ef4e17ed6169a320
Beni Mahat <benimahat6565@gmail.com>
2020-11-21 20:30:11 -0700

    Chaning whole page

---
+++ HWs/HW12/Profile/script.js
@@ -0,0 +1,21 @@
+$("#mealMatcherGit").click(function(){
+
+    window.open("https://github.com/benimahat1291/project_1-----MealMatcher.git");
+})
+
+$("#mealMatcherSite").click(function(){
+ 
+    window.open("https://benimahat1291.github.io/project_1-----MealMatcher/");
+})
+
+$("#ResumeBtn").click(function(){
+
+    window.open("img/resume.pdf");
+})
+
+$("#LinkedInBtn").click(function(){
+
+    window.open("https://www.linkedin.com/in/beni-mahat-1218b5184/");
+})
+
+
